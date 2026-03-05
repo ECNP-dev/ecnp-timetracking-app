c45ba2a9d17fc9ba910e9a2579f6fc259889bf54
ECNP-dev <a.parisella@ecnp.eu>
2026-03-05 16:16:26 +0100

    Update vite.config.js

--- electron-app/vite.config.js
+++ electron-app/vite.config.js
@@ -4,12 +4,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-
+  root: ".",
+  base: "",
   server: {
     port: 3000,
     strictPort: true
   },
-
   build: {
     outDir: "dist",
     emptyOutDir: true

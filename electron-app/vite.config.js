3c203744dfba3f5b76ac66a2556f4d46bc90eea4
ECNP-dev <a.parisella@ecnp.eu>
2026-03-05 09:32:12 +0100

    Update vite.config.js

--- electron-app/vite.config.js
+++ electron-app/vite.config.js
@@ -1,23 +1,9 @@
 
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  root: ".",                      // Project root
-  publicDir: "public",            // Location of index.html
-  build: {
-    outDir: "dist",               // Where Vite will output the frontend build
-    emptyOutDir: true,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-  server: {
-    port: 3000,
-    strictPort: true
-  }
+  server: { port: 3000, strictPort: true },
+  build: { outDir: "dist", emptyOutDir: true }
 });

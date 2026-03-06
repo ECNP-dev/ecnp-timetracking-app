fc47a62f24731ca686b6940db639e57bfa0ca970
ECNP-dev <a.parisella@ecnp.eu>
2026-03-06 13:14:00 +0100

    Update preload.js

--- electron-app/electron/preload.js
+++ electron-app/electron/preload.js
@@ -2,7 +2,6 @@
 // electron/preload.js
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electronMSAL", {
-  onRedirect: (callback) => ipcRenderer.on("msal-redirect", (e, url) => callback(url)),
-  notifyReady: () => ipcRenderer.send("renderer-ready")
+contextBridge.exposeInMainWorld("auth", {
+  login: () => ipcRenderer.invoke("auth:login")
 });

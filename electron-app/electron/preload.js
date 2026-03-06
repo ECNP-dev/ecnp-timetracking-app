e1db83c9d3679e8526637ab7258ce64b66b3bd8a
ECNP-dev <a.parisella@ecnp.eu>
2026-03-06 12:05:37 +0100

    Update preload.js

--- electron-app/electron/preload.js
+++ electron-app/electron/preload.js
@@ -1,10 +1,12 @@
-const { contextBridge, ipcRenderer } = require("electron");
+
+// electron/preload.js
+const { contextBridge } = require("electron");
 
 contextBridge.exposeInMainWorld("electronMSAL", {
-  onRedirect: (cb) => ipcRenderer.on("msal:redirect", (_, url) => cb(url))
-});
-
-contextBridge.exposeInMainWorld("electronAPI", {
-  onStartFromTray: (cb) => ipcRenderer.on("tray:start", cb),
-  onStopFromTray: (cb) => ipcRenderer.on("tray:stop", cb)
+  getRedirectUri: () => {
+    if (global.getMsalRedirectUri) {
+      return global.getMsalRedirectUri();
+    }
+    return null;
+  }
 });

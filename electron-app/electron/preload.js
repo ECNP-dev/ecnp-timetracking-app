dbd4ae5faf89acb79ecbf4bb73b2b910ba13cccf
ECNP-dev <a.parisella@ecnp.eu>
2026-03-10 11:47:39 +0100

    Update preload.js

--- electron-app/electron/preload.js
+++ electron-app/electron/preload.js
@@ -2,12 +2,7 @@
 // electron/preload.js
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electronMSAL", {
-  onRedirect: (cb) => ipcRenderer.on("msal-redirect", (_e, url) => cb(url)),
-  notifyReady: () => ipcRenderer.send("renderer-ready")
-});
-
-// App APIs
+// Core app APIs
 contextBridge.exposeInMainWorld("ecnp", {
   // Tasks
   loadTasks: () => ipcRenderer.invoke("tasks:load"),
@@ -19,9 +14,20 @@ contextBridge.exposeInMainWorld("ecnp", {
   // Reports
   generateMonthly: (year, month) => ipcRenderer.invoke("reports:generateMonthly", { year, month }),
 
-  // Settings / logs folder
+  // Settings and logs folder
   getLogsDir: () => ipcRenderer.invoke("settings:getLogsDir"),
   openLogsDir: () => ipcRenderer.invoke("settings:openLogsDir"),
   writeTestLog: () => ipcRenderer.invoke("settings:writeTestLog"),
-  overrideLogsDir: (requester, newPath) => ipcRenderer.invoke("settings:overrideLogsDir", { requester, newPath })
+  overrideLogsDir: (requester, newPath) =>
+    ipcRenderer.invoke("settings:overrideLogsDir", { requester, newPath }),
+
+  // Startup: launch app in tray mode
+  setLaunchTray: (enable) => ipcRenderer.invoke("settings:setLaunchTray", enable)
+});
+
+// Timer / tray bridge
+contextBridge.exposeInMainWorld("ecnpTimer", {
+  notifyState: (state) => ipcRenderer.send("timer:state", { state }),
+  onGlobalToggle: (cb) => ipcRenderer.on("global-toggle", cb),
+  trayHide: () => ipcRenderer.send("tray:hide")
 });
